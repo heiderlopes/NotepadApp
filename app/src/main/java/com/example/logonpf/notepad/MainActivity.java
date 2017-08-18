7955c77f3246b51b281cfb35f5244424b9f9c158
logonpf <logonpf@fiap.com.br>
2017-08-18 08:35:05 -0300

    Initial commit

---
+++ app/src/main/java/com/example/logonpf/notepad/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.logonpf.notepad;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.principal, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.salvar:
+                
+                return true;
+            case R.id.pesquisar:
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
